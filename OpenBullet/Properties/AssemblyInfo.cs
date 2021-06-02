a8a7b3135f515aab1636658d6f4991e7066a10e6
Josue <redhat086@gmail.com>
2021-06-01 19:46:38 -0500

    Primer Semestre

--- OpenBullet/Properties/AssemblyInfo.cs
+++ OpenBullet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("OpenBullet")]
+[assembly: AssemblyTitle("OpenBullet Primer Semestre")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenBullet")]
+[assembly: AssemblyProduct("OpenBullet Primer Semestre")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
